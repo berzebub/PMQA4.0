b2783d019426c167228c2b5111b861d335537f3e
MMasterZ <soulhunz@hotmail.com>
2020-10-31 19:35:56 +0700

    update assessment

--- src/pages/assessment.vue
+++ src/pages/assessment.vue
@@ -1,82 +1,90 @@
 <template>
   <q-page>
-    <div style="width:1200px;margin:auto;">
-      <div class="row relative-position ">
-        <div class="col-5  q-pa-lg" align="left">
-          <div id="container"></div>
-        </div>
-        <div class="col self-center  " align="center">
-          <span style="font-size:24px;">ผลดำเนินการ PMQA4.0 ในภาพรวม</span>
-          <br />
-          <span style="font-size:48px;">345 คะแนน</span>
-        </div>
-        <div class="absolute q-pa-md" style="right:0">
-          <q-btn round="" push="" class="bg-teal" @click="printBtn()">
-            <q-icon class="text-white" size="18px" name="fas fa-print"></q-icon>
-          </q-btn>
-        </div>
-      </div>
-
-      <q-separator class="bg-black" style="height:2px;"></q-separator>
-
-      <div class=" q-mt-lg ">
-        <!-- 1 -->
-        <div
-          class="row q-pb-lg q-pa-md "
-          v-for="(item, index) in dataList"
-          :key="index"
-        >
-          <div class="col-3 self-center q-pa-sm" align="center">
-            <q-icon
-              size="70px"
-              class="color1"
-              :name="item.fontawesome"
-            ></q-icon>
+    <div v-show="!isLoadData">
+      <div style="width:1200px;margin:auto;">
+        <div class="row relative-position ">
+          <div class="col-5  " align="center">
+            <div id="container"></div>
+          </div>
+          <div class="col self-center  " align="center">
+            <span style="font-size:24px;">ผลดำเนินการ PMQA4.0 ในภาพรวม</span>
             <br />
-            <br />
-            <span
-              class=" block"
-              align="left"
-              style="width:200px;font-size:24px;"
-              >{{ item.title }}</span
+            <span style="font-size:48px;"
+              >{{ parseInt(totalAvgScore) }} คะแนน</span
             >
           </div>
-          <div class="col q-pr-xl q-pb-xl self-end q-pa-sm ">
-            <div class="row q-px-lg">
-              <div
-                class="col self-end q-px-lg"
-                v-for="(score, index2) in item.score"
-                :key="index2"
+          <div class="absolute q-pa-md" style="right:0">
+            <q-btn round="" push="" class="bg-teal" @click="printBtn()">
+              <q-icon
+                class="text-white"
+                size="18px"
+                name="fas fa-print"
+              ></q-icon>
+            </q-btn>
+          </div>
+        </div>
+
+        <q-separator class="bg-black" style="height:2px;"></q-separator>
+
+        <div class=" q-mt-lg ">
+          <div
+            class="row q-pb-lg q-pa-md "
+            v-for="(item, index) in dataList"
+            :key="index"
+          >
+            <div class="col-3 self-center q-pa-sm" align="center">
+              <q-icon
+                size="70px"
+                class="color1"
+                :name="item.fontawesome"
+              ></q-icon>
+              <br />
+              <br />
+              <span
+                class=" block"
+                align="left"
+                style="width:200px;font-size:24px;"
+                >{{ item.title }}</span
               >
+            </div>
+            <div class="col q-pr-xl q-pb-xl self-end q-pa-sm ">
+              <div class="row q-px-lg">
                 <div
-                  class="bg5 relative-position"
-                  style="height:130px;"
-                  align="center"
+                  class="col self-end q-px-lg"
+                  v-for="(score, index2) in item.score"
+                  :key="index2"
                 >
-                  <span
-                    class="absolute-center text-white "
-                    style="font-size:24px;"
-                    >{{ score }}</span
+                  <div
+                    class="bg5 relative-position"
+                    :style="`height:${(130 / 500) * score || 2}px`"
+                    align="center"
                   >
-                  <span
-                    class="absolute-bottom"
-                    style="bottom:-45px;font-size:24px;"
-                    >{{ `${index + 1}.${index2 + 1}` }}</span
-                  >
+                    <span
+                      class="absolute-center text-white "
+                      style="font-size:24px;"
+                      >{{ score || "" }}</span
+                    >
+                    <span
+                      class="absolute-bottom"
+                      style="bottom:-45px;font-size:24px;"
+                      >{{ `${index + 1}.${index2 + 1}` }}</span
+                    >
+                  </div>
                 </div>
               </div>
+              <div style="border-bottom:2px solid"></div>
             </div>
-            <div style="border-bottom:2px solid"></div>
           </div>
         </div>
       </div>
+      <step-footer></step-footer>
     </div>
-    <step-footer></step-footer>
   </q-page>
 </template>
 
 <script>
 import stepFooter from "../components/footer";
+import Axios from "axios";
 export default {
   components: {
     stepFooter
@@ -92,58 +100,85 @@ export default {
         {
           title: `2. การวางแผน เชิงยุทธศาสตร์`,
           fontawesome: "fas fa-map-signs",
-          score: [200, 100, 400, 200]
+          score: [0, 0, 0, 0]
         },
         {
           title: `3. การให้ความสำคัญ กับผู้รับบริการและ ผู้มีส่วนได้ส่วนเสีย`,
           fontawesome: "fas fa-users",
-          score: [200, 100, 400, 200]
+          score: [0, 0, 0, 0]
         },
         {
           title: `4. การวัด การวิเคราะห์ และการจัดการความรู้`,
           fontawesome: "fas fa-chart-line",
-          score: [200, 100, 400, 200]
+          score: [0, 0, 0, 0]
         },
         {
           title: `5. การมุ่งเน้นบุคลากร`,
           fontawesome: "fas fa-users-cog",
-          score: [200, 100, 400, 200]
+          score: [0, 0, 0, 0]
         },
         {
           title: `6. การมุ่งเน้นระบบ ปฏิบัติการ`,
           fontawesome: "fas fa-project-diagram",
-          score: [200, 100, 400, 200]
+          score: [0, 0, 0, 0]
         },
         {
           title: `7. ผลลัพธ์การดำเนินการ`,
           fontawesome: "fas fa-trophy",
-          score: [200, 100, 400, 200]
+          score: [0, 0, 0, 0]
         }
-      ]
+      ],
+      totalAvgScore: 0,
+      isLoadData: true
     };
   },
   methods: {
     printBtn() {
-      this.$router.push("/print/assessment");
+      let result = this.$router.resolve({
+        path: "/print/assessment"
+      });
+
+      window.open(result.href, "__blank");
     },
     render() {
+      let avgScoreLst = [];
+
+      for (let i = 0; i < this.dataList.length; i++) {
+        avgScoreLst.push(
+          this.dataList[i].score.map(x => x).reduce((a, b) => a + b, 0) / 4
+        );
+      }
+
+      this.totalAvgScore = avgScoreLst.reduce((a, b) => a + b, 0) / 7;
+
+      let setPrintData = {
+        data: this.dataList,
+        avgScore: avgScoreLst,
+        totalAvgScore: parseInt(this.totalAvgScore)
+      };
+
+      this.$q.sessionStorage.set("printData", setPrintData);
+
       Highcharts.chart("container", {
         chart: {
           polar: true,
-          width: "500",
-          height: "450",
+          height: "500",
           style: {
             fontFamily: "PROMPT-R"
           }
         },
 
+        accessibility: {
+          description: ""
+        },
+
         title: {
-          text: "",
+          text: "Budget vs spending",
           x: -80
         },
 
         pane: {
-          size: "85%"
+          size: "80%"
         },
 
         xAxis: {
@@ -171,19 +206,26 @@ export default {
         yAxis: {
           gridLineInterpolation: "polygon",
           lineWidth: 0,
-          min: 100,
+          min: 0,
+          max: 500,
           gridLineColor: "#000000"
         },
 
         tooltip: {
           shared: true,
-          pointFormat: ""
+          pointFormat: "<span >คะแนน: <b>{point.y:,.0f}</b><br/>"
+        },
+
+        legend: {
+          align: "right",
+          verticalAlign: "middle",
+          layout: "vertical"
         },
 
         series: [
           {
-            name: "Allocated Budget",
-            data: [1000, 200, 300, 400, 500, 600, 700],
+            name: "",
+            data: [500, 400, 100, 400, 400, 400, 500],
             pointPlacement: "on",
             showInLegend: false,
             type: "area",
@@ -191,14 +233,115 @@ export default {
             borderColor: "#009688"
           }
         ],
-        credits: {
-          enabled: false
+
+        responsive: {
+          rules: [
+            {
+              condition: {
+                maxWidth: 500
+              },
+              chartOptions: {
+                legend: {
+                  align: "center",
+                  verticalAlign: "bottom",
+                  layout: "horizontal"
+                },
+                pane: {
+                  size: "70%"
+                }
+              }
+            }
+          ]
         }
       });
+
+      this.isLoadData = false;
+    },
+    async getData() {
+      console.clear();
+      const url = this.apiPath + "user/getAllCategory1_6.php";
+
+      const postData = {
+        year: this.$q.sessionStorage.getItem("y"),
+        user_id: this.$q.sessionStorage.getItem("uid")
+      };
+
+      let getData = await Axios.post(url, postData);
+
+      getData = getData.data;
+
+      for (let i = 0; i < this.dataList.length; i++) {
+        for (let ii = 0; ii < this.dataList[i].score.length; ii++) {
+          let setScore = 0;
+
+          let getBasic = getData.filter(
+            x => x.step == i + 1 && x.mode == "basic" && x.q_number == ii + 1
+          );
+
+          let getAdvance = getData.filter(
+            x => x.step == i + 1 && x.mode == "advance" && x.q_number == ii + 1
+          );
+
+          let getSignificance = getData.filter(
+            x =>
+              x.step == i + 1 &&
+              x.mode == "significance" &&
+              x.q_number == ii + 1
+          );
+
+          let getBasicScore = 0;
+          let getAdvanceScore = 0;
+          let getSignificanceScore = 0;
+
+          let sumScore = 0;
+
+          if (getBasic.length) {
+            getBasicScore = getBasic[0].check_box
+              .split(",")
+              .every(x => x == "1")
+              ? 300
+              : 0;
+          }
+
+          if (getBasicScore != 0) {
+            setScore = 300;
+
+            if (getAdvance.length) {
+              getAdvanceScore = getAdvance[0].check_box
+                .split(",")
+                .every(x => x == "1")
+                ? 400
+                : 0;
+            }
+
+            if (getAdvanceScore != 0) {
+              setScore = 400;
+
+              if (getSignificance.length) {
+                getSignificanceScore = getSignificance[0].check_box
+                  .split(",")
+                  .every(x => x == "1")
+                  ? 500
+                  : 0;
+              }
+
+              if (getSignificanceScore != 0) {
+                setScore = 500;
+              }
+            }
+          } else {
+            setScore = 0;
+          }
+
+          this.dataList[i].score[ii] = setScore;
+        }
+      }
+
+      this.render();
     }
   },
   mounted() {
-    this.render();
+    this.getData();
   }
 };
 </script>
--- src/pages/printAssessment.vue
+++ src/pages/printAssessment.vue
@@ -1,9 +1,303 @@
 <template>
-  <div></div>
+  <div class="bg-print">
+    <div class="pageA4">
+      <table>
+        <thead>
+          <tr>
+            <td>
+              <div class="relative-position">
+                <div class="row bg1 container-header">
+                  <div class="col  self-center" align="right"></div>
+                </div>
+                <div class="row bg2 container-header relative-position">
+                  <div
+                    class="col-1 self-center q-px-md"
+                    align="right"
+                    style="width: 150px;"
+                  ></div>
+                  <div class="col  self-center q-px-md" align="right">
+                    <span class="font-16">
+                      กองควบคุมโรคและภัยสุขภาพในภาวะฉุกเฉิน
+                    </span>
+                  </div>
+                  <div
+                    class="col-1  self-center q-px-md"
+                    align="right"
+                    style="width: 150px;"
+                  >
+                    <span class="font-16">
+                      ปี 2563
+                    </span>
+                  </div>
+                </div>
+                <div class="absolute-bottom" style="left: 20px; bottom: -7px;">
+                  <q-img
+                    src="../../public/images/logo.png"
+                    width="200px"
+                  ></q-img>
+                </div>
+              </div>
+            </td>
+          </tr>
+        </thead>
+
+        <tbody>
+          <tr v-for="(item, index) in dataList" :key="index">
+            <td>
+              <div class="row q-mt-md ">
+                <div class="col-3 self-center q-pa-sm" align="center">
+                  <q-icon
+                    size="40px"
+                    class="color1"
+                    :name="item.fontawesome"
+                  ></q-icon>
+                  <br />
+                  <br />
+                  <span
+                    class=" block"
+                    align="left"
+                    style="width:150px;font-size:16px;"
+                    >{{ item.title }}</span
+                  >
+                </div>
+                <div class="col q-pb-lg  self-end ">
+                  <div class="row">
+                    <div
+                      class="col self-end"
+                      :class="index + 1 == 7 ? 'q-px-sm' : ' q-px-md'"
+                      v-for="(score, index2) in item.score"
+                      :key="index2"
+                    >
+                      <div
+                        class="bg5 relative-position"
+                        :style="`height:${(85 / 500) * score || 2}px`"
+                        align="center"
+                      >
+                        <span
+                          class="absolute-center text-white "
+                          style="font-size:16px;"
+                          >{{ score || "" }}</span
+                        >
+                        <span
+                          class="absolute-bottom"
+                          style="bottom:-35px;font-size:16px;"
+                          >{{ `${index + 1}.${index2 + 1}` }}</span
+                        >
+                      </div>
+
+                      <div class="break"></div>
+                    </div>
+                  </div>
+                  <div style="border-bottom:2px solid"></div>
+                </div>
+                <div
+                  class="col-1 self-center "
+                  style="width:100px;"
+                  align="center"
+                >
+                  <span>
+                    <span class="block" style="font-size:32px">{{
+                      item.score.reduce((a, b) => a + b, 0) / 4
+                    }}</span>
+                    <span class="block" style="margin-top:-10px;font-size:12px"
+                      >คะแนน</span
+                    >
+                  </span>
+                </div>
+              </div>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+
+    <div class="break"></div>
+
+    <div class="pageA4 ">
+      <div class="relative-position">
+        <div class="row bg1 container-header">
+          <div class="col  self-center" align="right"></div>
+        </div>
+        <div class="row bg2 container-header relative-position">
+          <div
+            class="col-1 self-center q-px-md"
+            align="right"
+            style="width: 150px;"
+          ></div>
+          <div class="col  self-center q-px-md" align="right">
+            <span class="font-16">
+              กองควบคุมโรคและภัยสุขภาพในภาวะฉุกเฉิน
+            </span>
+          </div>
+          <div
+            class="col-1  self-center q-px-md"
+            align="right"
+            style="width: 150px;"
+          >
+            <span class="font-16">
+              ปี 2563
+            </span>
+          </div>
+        </div>
+        <div class="absolute-bottom" style="left: 20px; bottom: -7px;">
+          <q-img src="../../public/images/logo.png" width="200px"></q-img>
+        </div>
+      </div>
+      <div class="q-my-xl row q-px-xl " align="center" style="font-size:30px;">
+        <div align="center">ผลดำเนินการ PMQA4.0 ในภาพรวม</div>
+        <q-space></q-space>
+        <div align="center">{{ totalAvgScore }} คะแนน</div>
+      </div>
+      <div align="center">
+        <div id="container"></div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      dataList: this.$q.sessionStorage.getItem("printData").data,
+      totalAvgScore: this.$q.sessionStorage.getItem("printData").totalAvgScore
+    };
+  },
+  methods: {
+    render() {
+      Highcharts.chart("container", {
+        chart: {
+          polar: true,
+          height: "500",
+          style: {
+            fontFamily: "PROMPT-R"
+          }
+        },
+
+        title: {
+          text: "",
+          x: -80
+        },
+
+        pane: {
+          size: "85%"
+        },
+
+        xAxis: {
+          useHTML: false,
+          categories: [
+            "หมวด 1",
+            "หมวด 2",
+            "หมวด 3",
+            "หมวด 4",
+            "หมวด 5",
+            "หมวด 6",
+            "หมวด 7"
+          ],
+          tickmarkPlacement: "on",
+          lineWidth: 0,
+          labels: {
+            style: {
+              fontSize: "16px",
+              color: "#000000"
+            }
+          },
+          gridLineColor: "transparent"
+        },
+
+        yAxis: {
+          gridLineInterpolation: "polygon",
+          lineWidth: 0,
+          min: 100,
+          max: 500,
+          gridLineColor: "#000000"
+        },
+
+        tooltip: {
+          shared: true,
+          pointFormat: ""
+        },
+
+        series: [
+          {
+            name: "",
+            data: this.$q.sessionStorage.getItem("printData").avgScore,
+            pointPlacement: "on",
+            showInLegend: false,
+            type: "area",
+            color: "#79BDA3",
+            borderColor: "#009688"
+          }
+        ],
+        credits: {
+          enabled: false
+        }
+      });
+    }
+  },
+  mounted() {
+    this.render();
+
+    setTimeout(() => {
+      // window.print();
+      // window.close();
+    }, 1500);
+  }
+};
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+table {
+  width: 100%;
+  border-collapse: collapse;
+}
+
+.bg-print {
+  width: 100%;
+  height: fit-content;
+  min-height: 100vh;
+  background-color: rgba(5, 5, 5, 0.5);
+  padding: 15px;
+}
+
+.pageA4 {
+  position: relative;
+  width: 210mm;
+  min-height: 297mm;
+  height: fit-content;
+  background-color: #fff;
+  margin: 15px auto;
+  box-shadow: 5px 0px 10px 2px #555;
+}
+
+.container-header {
+  height: 40px;
+}
+
+.break {
+  break-after: page;
+}
+
+@media print {
+  .bg-print {
+    height: fit-content;
+    background-color: #fff;
+    padding: 0px;
+  }
+
+  .pageA4 {
+    width: 210mm;
+    height: fit-content;
+    background-color: #fff;
+    margin: 0px auto;
+    box-shadow: 0px 0px 0px 0px #555;
+  }
+
+  @page {
+    size: A4;
+    margin: 0mm;
+    width: 210mm;
+    height: 297mm;
+  }
+}
+</style>
